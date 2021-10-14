55281f21d38daf805875780558f60882274cf08a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-10-14 10:44:29 +0100

    regular commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,16 @@
+const heroPatterns = require("tailwind-heropatterns")({
+    variants: [],
+    patterns: ["death-star", "hexagons", "circuit-board", "polka-dots", "signal"],
+    colors: {
+        default: "#9C92AC",
+        "white": "#FFFFFF"
+    },
+    opacity: {
+        default: "0.4",
+        "100": "1.0"
+    }
+});
+
 module.exports = {
   purge: [ "*.html" ],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +20,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ heroPatterns ],
 }
