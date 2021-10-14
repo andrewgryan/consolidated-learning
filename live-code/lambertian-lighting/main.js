f0320b4d6a3e0d6aaf823de728eb7a7642a117e8
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-10-14 17:18:05 +0100

    implement lambert light model

--- live-code/lambertian-lighting/main.js
+++ live-code/lambertian-lighting/main.js
@@ -17,20 +17,28 @@ const drawBunny = regl({
 
         attribute vec3 position, normal;
         uniform mat4 model, view, projection;
+        uniform vec3 lightPosition;
         varying vec3 vNormal;
+        varying vec3 vPosition;
 
         void main() {
             vNormal = normal;
-            gl_Position = projection * view * model * vec4(position, 1.0);
+            vec4 worldPosition = model * vec4(position, 1.0);
+            vPosition = worldPosition.xyz;
+            gl_Position = projection * view * worldPosition;
         }
     `,
     frag: `
         precision mediump float;
 
-        varying vec3 vNormal;
+        varying vec3 vNormal, vPosition;
+        uniform vec3 lightPosition;
 
         void main() {
-            gl_FragColor = vec4(vNormal, 1.0);
+            vec3 color = vec3(0.5, 0.5, 0.0);
+            float intensity = max(0.0, dot(vNormal, normalize(lightPosition - vPosition)));
+            float ambient = 0.2;
+            gl_FragColor = vec4(ambient + intensity * color, 1.0);
         }
     `,
     attributes: {
@@ -38,7 +46,8 @@ const drawBunny = regl({
         normal: angleNormals(bunny.cells, bunny.positions)
     },
     uniforms: {
-        model: ({ time }) => mat4.fromRotation([], 0.1 * time * Math.PI, [0, 1, 0])
+        model: ({ time }) => mat4.fromRotation([], Math.PI / 2, [0, 1, 0]),
+        lightPosition: ({ time }) => [0, 10 * Math.cos(time), 10 * Math.sin(time)]
     },
     elements: bunny.cells,
     count: bunny.cells.length * 3
